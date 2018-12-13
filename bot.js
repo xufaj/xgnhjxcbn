0ded956ea6821234c1a1363c864bdd54262d43f4
xufaj <45463607+xufaj@users.noreply.github.com>
2018-12-13 19:02:40 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,34 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+var prefix = "8";
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);  
+});
+
+client.on('ready', async() => {
+var server = "513756427190403072"; // ايدي السررفر
+var channel = "520945700448763905";//ايدي الروم
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('** SPAM . SPAM . SPAM . SPAM . SPAM . SPAM . SPAM . SPAM . SPAM . SPAM . SPAM . SPAM . SPAM . SPAM . SPAM . SPAM . SPAM . SPAM . SPAM . SPAM . SPAM . SPAM . SPAM . SPAM . SPAM . SPAM . SPAM . SPAM . SPAM . SPAM .  **')
+    },999);
+})
+
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "say") {
+if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('?|**\`ADMINISTRATOR\`ليس لديك صلاحيات`**');
+   message.channel.sendMessage(args.join("  "))
+   message.delete()
+  }
+ });
+
+ 
+client.login(process.env.BOT_TOKEN);
